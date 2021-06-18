073c263b79a3deeb62ab0e26a5d60f2642e09ba6
cunhang_wwei <cunhang_wwei@yunzhijia.com>
2021-06-18 18:49:21 +0800

    feat: 即将完成diff

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: cunhang_wwei
  * @Date: 2021-06-12 10:07:50
- * @LastEditTime: 2021-06-16 20:30:21
+ * @LastEditTime: 2021-06-18 18:47:51
  * @LastEditors: cunhang_wwei
  * @Description: In User Settings Edit
  */
@@ -11,11 +11,11 @@ import patch from './patch'
 
 const test = h('h1', {}, '虚拟节点上树了！')
 
-const test2 = h('ul', {}, [
-    h('li', {key: '1'}, '1'),
-    h('li', {key: '2'}, '2'),
-    h('li', {key: '3'}, '3'),
-    h('li', {key: '4'}, '4')
+const base = h('ul', {}, [
+    h('li', { key: '1' }, '1'),
+    h('li', { key: '2' }, '2'),
+    h('li', { key: '3' }, '3'),
+    h('li', { key: '4' }, '4')
 ])
 
 const test3 = h('div', {}, '剑圣')
@@ -28,24 +28,67 @@ const test4 = h('div', {}, [
     ])
 ])
 
+// 测试 是同一个虚拟节点的情况
 const test5 = h('ul', {}, [
-    h('li', {key: '4'}, '444444444'),
-    h('li', {key: '3'}, '3'),
-    h('li', {key: '2'}, '222222'),
-    h('li', {key: '1'}, '1')
+    h('li', { key: '4' }, '444444444'),
+    h('li', { key: '3' }, '3'),
+    h('li', { key: '2' }, '222222'),
+    h('li', { key: '1' }, '1')
 ])
 
-// console.log('test', test)
-// console.log('test2', test2)
-// console.log('test3', test3)
+// 测试 while循环结束 增加节点的情况
+const test6 = h('ul', {}, [
+    h('li', { key: '1' }, '1'),
+    h('li', { key: '2' }, '2'),
+    h('li', { key: '3' }, '3'),
+    h('li', { key: '4' }, '4'),
+    h('li', { key: '5' }, '5'),
+    h('li', { key: '6' }, '6')
+])
+
+// 测试 while循环结束 删除节点的情况
+const test7 = h('ul', {}, [
+    h('li', { key: '1' }, '1'),
+    h('li', { key: '2' }, '2'),
+])
+
+// 测试 while循环, 4种命中类型都不命中的情况
+const test8 = h('ul', {}, [
+    h('li', { key: '3' }, '3'),
+    h('li', { key: '1' }, '1'),
+    h('li', { key: '4' }, '4'),
+    h('li', { key: '2' }, '2')
+])
+
+// 测试 while循环, 4种命中类型都不命中的情况
+const test9 = h('ul', {}, [
+    h('li', { key: '3' }, '3'),
+    h('li', { key: '1' }, '1'),
+    h('li', { key: '4' }, '4'),
+    h('li', { key: 'A' }, 'B'),
+    h('li', { key: 'qqq2' }, 'qqq2'),
+    h('li', { key: 'hahahh2' }, 'hahahh2'),
+])
+
+
+// 测试 while循环结束 增加节点的情况
+const test10 = h('ul', {}, [
+    h('li', { key: '5' }, '5'),
+    h('li', { key: '6' }, '6'),
+    h('li', { key: '1' }, '1'),
+    h('li', { key: '2' }, '2'),
+    h('li', { key: '3' }, '3'),
+    h('li', { key: '4' }, '4'),
+])
+
 
 const container = document.getElementById('container')
 const btn = document.getElementById('btn')
 
 // 虚拟节点第一次上树
-patch(container, test2)
+patch(container, base)
 
 btn.onclick = function () {
     // 虚拟节点第二次上树
-    patch(test2, test5)
+    patch(base, test9)
 }
--- src/updateChildren.js
+++ src/updateChildren.js
@@ -1,9 +1,10 @@
 /*
  * @Date: 2021-06-15 14:19:35
  * @LastEditors: cunhang_wwei
- * @LastEditTime: 2021-06-16 20:32:48
+ * @LastEditTime: 2021-06-18 18:48:40
  * @Description: diff算法 四命中 更新子节点
  */
+import createElement from './createElement'
 import isSameVnode from './isSameVnode'
 import patchVnode from './patchVnode'
 
@@ -18,19 +19,31 @@ export default function (parentElm, oldCh, newCh) {
     let oldStartVnode = oldCh[0]
     // 旧后
     let oldEndVnode = oldCh[oldEndIndex]
-    
+
     // 新前
     let newStartIndex = 0
     // 新后
     let newEndIndex = newCh.length - 1
     // 新前节点
-    let newStartVnode = newCh[0] 
+    let newStartVnode = newCh[0]
     // 新后节点
     let newEndVnode = newCh[newEndIndex]
 
+    // 老虚拟节点的map
+    let oldKeyMap = {}
+
     while (newStartIndex <= newEndIndex && oldStartIndex <= oldEndIndex) {
         console.log('❤')
-        if (isSameVnode(oldStartVnode, newStartVnode)) {
+        // 首先跳过null和undefined的节点
+        if (oldStartVnode === null || oldStartVnode === undefined) {
+            oldStartVnode = oldCh[++oldStartIndex]
+        } else if (oldEndVnode === null || oldEndVnode === undefined) {
+            oldEndVnode = oldCh[--oldEndIndex]
+        } else if (newStartVnode === null || newStartVnode === undefined) {
+            newStartVnode = newCh[++newStartIndex]
+        } else if (newEndVnode === null || newEndVnode === undefined) {
+            newEndVnode = newCh[--newEndIndex]
+        } else if (isSameVnode(oldStartVnode, newStartVnode)) {
             // 新前和旧前
             console.log('①新前和旧前命中')
             patchVnode(oldStartVnode, newStartVnode)
@@ -49,22 +62,71 @@ export default function (parentElm, oldCh, newCh) {
 
             // 移动节点，将当前的旧前节点移动到旧后节点的下一个兄弟节点之前
             parentElm.insertBefore(oldStartVnode.elm, oldEndVnode.elm.nextSibling)
-            
-            oldStartVnode = oldCh[++newStartIndex]
-            newEndVnode = newCh[--oldEndIndex]
+
+            oldStartVnode = oldCh[++oldStartIndex]
+            newEndVnode = newCh[--newEndIndex]
         } else if (isSameVnode(oldEndVnode, newStartVnode)) {
             // 旧后和新前
             console.log('④旧后和新前命中')
             patchVnode(oldEndVnode, newStartVnode)
             // 移动节点，将当前的旧后节点移动到旧前节点之前
             parentElm.insertBefore(oldEndVnode.elm, oldStartVnode.elm)
-            
+
             oldEndVnode = oldCh[--oldEndIndex]
             newStartVnode = newCh[++newStartIndex]
         } else {
-            console.log('5')
-            // 上述四种情况都不命中
-            // return
+            // 上述四种情况都不命中,需要用循环来寻找，移动到 oldStartVnode 前面
+
+            // 制作一个旧列表的key-index映射表
+            if (!Object.keys(oldKeyMap).length) {
+                for (let index = oldStartIndex; index <= oldEndIndex; index++) {
+                    if (oldCh[index]) {
+                        const { key } = oldCh[index]
+                        if (key !== undefined) {
+                            oldKeyMap[key] = index
+                        }
+                    }
+                }
+                console.log('oldKeyMap', oldKeyMap)
+            }
+
+            const indexInOld = oldKeyMap[newStartVnode.key]
+            if (indexInOld === undefined) {
+                // 是 undefined， 则是全新的项，需要将新节点插入
+                parentElm.insertBefore(createElement(newStartVnode), oldStartVnode.elm)
+            } else {
+                // 不是 undefined，则需要移动
+                let elmToMove = oldCh[indexInOld]
+                patchVnode(elmToMove, newStartVnode)
+
+                oldCh[indexInOld] = undefined
+
+                parentElm.insertBefore(elmToMove.elm, oldStartVnode.elm)
+            }
+
+            // 移动新列表的头指针
+            newStartVnode = newCh[++newStartIndex]
+        }
+    }
+
+    // 循环结束
+    if (oldStartIndex < oldEndIndex) {
+        console.log('旧前小于旧后，则说明 需要删除节点')
+        // 旧前小于旧后，则说明 需要删除节点
+        for (let i = oldStartIndex; i <= oldEndIndex; i++) {
+            if (oldCh[i]) {
+                parentElm.removeChild(oldCh[i].elm)
+            }
+        }
+    } else if (newStartIndex < newEndIndex) {
+        console.log('新前小于新后，则说明 需要新增节点')
+        // 新前小于新后，则说明 需要新增节点
+
+        // 遍历新的newCh,添加到oldCh没有处理的节点之前
+        for (let i = newStartIndex; i <= newEndIndex; i++) {
+            // 将新的虚拟节点变为DOM，并加到最后
+            const item = createElement(newCh[i])
+            parentElm.insertBefore(item, oldStartVnode.elm)
         }
     }
 }
